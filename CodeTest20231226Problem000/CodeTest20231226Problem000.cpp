4652969512b78622bcf60f5abfc7be2bd4249725
Beomin <960322ksh@hanmail.net>
2023-12-29 15:46:06 +0900

    백준 구간합 알고리즘(누적합) 연습

---
+++ CodeTest20231226Problem000/CodeTest20231226Problem000.cpp
@@ -0,0 +1,34 @@
+﻿// 백준 구간 합 구하기 4 실버3
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int num;
+    int snum;
+    cin >> num;
+    cin >> snum;
+    vector<int> nums(num);
+    vector<int> sums(num+1);
+    sums[0] = 0;
+
+    for (int i = 0; i < num; ++i)
+    {
+        cin >> nums[i];
+        sums[i + 1] = sums[i] + nums[i];
+    }
+
+    for (int i = 0; i < snum; ++i)
+    {
+        int L;
+        int R;
+        cin >> L;
+        cin >> R;
+        int answer = sums[R] - sums[L - 1];
+        cout << answer<<"\n" ;
+
+    }
+}
