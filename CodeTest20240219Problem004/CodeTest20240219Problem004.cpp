2aafa308da18480306bde47999beddd1a1090115
Beomin <960322ksh@hanmail.net>
2024-03-03 01:39:17 +0900

    // 백준 2157 여행 // 로직은 맞게 짜놓고 잔실수로 너무 오래푼 문제

---
+++ CodeTest20240219Problem004/CodeTest20240219Problem004.cpp
@@ -0,0 +1,20 @@
+﻿// CodeTest20240219Problem004.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+// 전부 0으로 초기화
+// 좌표에 대한 
+int main()
+{
+	int room;
+	cin >> room;
+	vector<vector<int>> rooms(room+1,vector<int>(room+1,0));//0불꺼짐
+	int board;
+	cin >> board;
+	for (int i = 0; i < board; ++i)
+	{
+
+	}
+}
---
+++ CodeTest20240226Problem001/CodeTest20240226Problem001.cpp
@@ -0,0 +1,62 @@
+﻿// 백준 2157번 여행
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+// 바텀업, dfs, 방문처리, dp
+// 다풀어놓고 잔실수로 너무 오래 풀었다.
+using namespace std; 
+int Caching(vector<vector<pair<int, int>>>& _routes, vector<vector<int>>& _cache, int _jump, int _citynum, int _dst, int _max);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int c, v, k;
+    cin >> c >> v >> k;
+
+    vector<vector<pair<int,int>>> routes(c+1);
+    for (int i = 0; i < k; ++i)
+    {
+        int sr, de, sc;
+        cin >> sr >> de >> sc;
+        routes[sr].push_back({ de,sc });
+    }
+    vector<vector<int>> cache(c+1,vector<int>(v+1,0));// [도시번호];
+    int answer=Caching(routes,cache,1,1,c,v+1);
+
+    cout << answer;
+}
+
+int Caching(vector<vector<pair<int, int>>>& _routes, vector<vector<int>>& _cache, int _jump, int _citynum,int _dst,int _max)
+{
+
+    if (_jump >= _max)
+    {
+        return INT_MIN;
+    }
+    if (_citynum == _dst)
+    {
+        return 0;
+    }
+
+
+    if (_cache[_citynum][_jump] != 0)
+    {
+        return _cache[_citynum][_jump];
+    }
+    else
+    {
+        _cache[_citynum][_jump] = INT_MIN;
+        for (auto nextcity : _routes[_citynum])
+        {
+            int ncnum = nextcity.first;
+            int nval = nextcity.second;
+            if (ncnum > _citynum)
+            {
+                _cache[_citynum][_jump] = max(_cache[_citynum][_jump],Caching(_routes,_cache,_jump+1,ncnum,_dst,_max)+nval);
+            }
+        }
+        return _cache[_citynum][_jump];
+    }
+}
