ff313b9eb13aed70db5bca1a6c12c6416c38eb6e
Beomin <960322ksh@hanmail.net>
2024-04-04 17:57:37 +0900

    // 백준 14572 스터디그룹 // 플레티넘, 비트로 풀이

--- CodeTest20240325Problem004/CodeTest20240325Problem004.cpp
+++ CodeTest20240325Problem004/CodeTest20240325Problem004.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <map>
+#include <cmath>
+#include <bitset>
 #include <algorithm>
 using namespace std;
 int sn, ak, d = 0;
-
+int effi=0;
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -14,52 +15,74 @@ int main()
     cout.tie(NULL);
 
     cin >> sn >> ak >> d;
-    vector<int> studentIndex;
-    vector<pair<int,pair<int,vector<int>>>> studentD; // 실력,학생 번호; 실력 내림차순정리
-    studentD.reserve(sn);
-    vector<vector<int>> studentAL(sn); // i번학생이 가지고잇는 알고리즘들 [i]{};
-    
-    set<int> group;
-    int stdnum;
-    for (int i = 0; i < sn; ++i)
-    {
-        int san, sd = 0;
-        cin >> san >> sd;
-        studentD.push_back({ sd,{i,{}} });
-        for (int j = 0; j < san; ++j)
-        {
-            int sa;
-            cin >> sa;
-            studentAL[i].push_back(sa);
-        }
-    }
+    vector<pair<int,bitset<30>>> students(sn);
+   // 각 학생들이 아는 알고리즘 비트(int형 한칸=> 알고있는 알고리즘, 알고있는 알고리즘 n에 대해서 & 2의n승)
+   for (int i = 0; i < sn; ++i)
+   {
+       int san, score;
+       cin >> san >> score;
+       students[i].first = score;
+       for (int j = 0; j < san; ++j)
+       {
+           int a;
+           cin >> a; 
+           students[i].second |= (1ULL << (a-1));// i학생이 a알고리즘을 알고있다: i번째 인트의 자리수 1활성화
+       }
+   }
+   sort(students.begin(), students.end(), [&](auto& a,auto& b)->bool {
+       if(a.first == b.first)
+           return a.second.to_ulong() < b.second.to_ulong();
+       return a.first < b.first; });
 
-    sort(studentD.begin(), studentD.end(), [&](auto& a, auto& b)->bool
-        {
-            if (a.first == b.first)
-                return a.second.first < b.second.first;
-            return a.first < b.first;
-        });
-
+   // 그룹내 학생들이 아는 비트
+   // 그룹내 학생들이 동시에 알고 있는 비트
+   
     int low = 0; 
     int high = 0;
-
-    while (high< sn)
+    bitset<30> alla=students[low].second;
+    bitset<30> aka=students[low].second;
+    while (high < sn)
     {
-        if (studentD[high].first - studentD[low].first > 10)
-        {              
-            // 그룹 변경
-             
-            // 인덱스 변경 // 낮은칸 하나 증가=> 증가 해도 같은 실력 다른 학생일 경우?
+        if (students[high].first- students[low].first<=d)
+        {// 그룹 가입 가능, 효율성 계산
+            alla |= students[high].second;
+            aka &= students[high].second;
+            bitset<30> group = alla & (~aka);
+            int count = 0;
+            for (int i =0;i<group.size();++i)
+            {
+                if (group[i] == 1)
+                    count++;
+            }
+            effi = max(effi, count * (high - low + 1));
+            high++;
         }
         else
-        {
-            // 그룹에 넣고 
-            
-            // 인덱스 변환
+        {// 그룹 변경
+            while (students[high].first - students[low].first > d)
+                low++;
+            // 점수 범위안에 드는 가장 낮은 학생
+            // 현재 그룹 계산
+            alla = students[low].second;
+            aka = students[low].second;
+            int curlow = low;
+            while (curlow <= high)
+            {
+                alla |= students[curlow].second;
+                aka &= students[curlow].second;
+                curlow++;
+            }
+            bitset<30> group = alla & (~aka);
+            int count = 0;
+            for (int i = 0; i < group.size(); ++i)
+            {
+                if (group[i] == 1)
+                    count++;
+            }
+            effi = max(effi, count * (high - low + 1));
             high++;
         }
     }
-        
+    cout << effi;
 }
 
