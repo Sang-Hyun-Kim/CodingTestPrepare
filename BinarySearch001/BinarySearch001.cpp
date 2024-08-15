b2a20071df07d683e822862794f0abad0169c0ee
Beomin <960322ksh@hanmail.net>
2024-08-15 17:50:50 +0900

    프로그래머스 임국심사 이분탐색

---
+++ BinarySearch001/BinarySearch001.cpp
@@ -0,0 +1,50 @@
+﻿// BinarySearch001.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+// 아오 times의 원소 가 int 이니까 (long long) 형변환하기, long long 끼리 연산하다 터질 수 있으니 mid 값 구할 때 센스 있게 구하기
+// 진짜 중요한 이분탐색 시에 mid 값과 low, high 값 잘 고르기
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    // low :1분, high: times의 원소중 max * 사람의 수(N) 
+    // mid 값이 정해졌을 때 N의 값과 비교
+    // mid분 동안 몇 명을 처리해야했는지 mid 분  나누기 time(i)를 원소의 수 만큼 ㄱㄱ
+    long long low = 1;
+    sort(times.begin(), times.end());
+    long long high = (long long)times.back() * n;
+
+    // 첫 mid
+    long long mid = 0;
+
+    while (low < high)
+    {
+        mid = low + (high - low) / 2;
+        long long people = 0;
+        for (auto time : times)
+        {
+            people += (mid / (long long)time);
+        }
+        if (n <= people)
+        {
+            high = mid;
+        }
+        else// n > people
+        {
+            low = mid + 1;
+        }
+
+    }
+    answer = low;
+    return answer;
+}
+
+int main()
+{
+    long long answer = solution(6, { 7,10 });
+    std::cout << answer;
+}
---
+++ CodeTest20240730Problem001/CodeTest20240730Problem001.cpp
@@ -0,0 +1,65 @@
+﻿// 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+int cand = 0;
+int pick = 0;
+int tot = 0;
+int maxi = -1;
+void BackT(int num, int left, set<int> curcan, vector<vector<int>>& candis);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> cand >> pick >> tot;
+
+    vector<vector<int>> candis(tot, vector<int>(2, 0));
+    for (int i = 0; i < cand; ++i)
+    {
+        cin >> candis[i][0] >> candis[i][1];
+    }
+    set<int> curcan;
+    BackT(0, pick, curcan, candis);
+
+    cout << maxi << endl;
+}
+
+void BackT(int num, int left, set<int> curcan, vector<vector<int>>& candis)
+{
+    if (num >= cand)
+    {// 마지막 후보까지 끝
+        return;
+    }
+
+    // 이번 후보 넣기
+    curcan.insert(num);
+    left -= 1;
+    if (left == 0)
+    {// k개의 후보를 다 골랐다면
+        // 계산
+        int pow = 0;
+        int speed = 0;
+        for (auto& a : curcan)
+        {
+            pow += candis[a][0];
+            speed += candis[a][1];
+
+        }
+        int sum = pow * speed;
+        maxi = max(maxi, sum);
+    }
+    else
+    {
+        BackT(num + 1, left, curcan, candis);
+    }
+    // 이전 계산 롤백
+    curcan.erase(curcan.find(num));
+    left += 1;
+    // 이번 후보 거르기 => 다음 후보 탐색 시작
+    BackT(num + 1, left, curcan, candis);
+
+}
