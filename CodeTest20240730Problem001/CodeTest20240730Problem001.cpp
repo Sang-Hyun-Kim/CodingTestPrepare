83c8311a96ccf8bd63f089491bd4b8ab29fb96df
Beomin <960322ksh@hanmail.net>
2024-08-15 20:28:39 +0900

    백준 26607 시로코 은행털이 DP 배낭 문제 힌트 보고 품 치명적인 배열 선언 실수

--- CodeTest20240730Problem001/CodeTest20240730Problem001.cpp
+++ CodeTest20240730Problem001/CodeTest20240730Problem001.cpp
@@ -1,65 +1,60 @@
-﻿// 
+﻿// 백준 26607번 시로코와 은행털기
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
-using namespace std;
 int cand = 0;
 int pick = 0;
 int tot = 0;
-int maxi = -1;
-void BackT(int num, int left, set<int> curcan, vector<vector<int>>& candis);
-int main()
+bool dp[80][81][16001] = {false,};// [index]][고른 수][최고A능력치] 가 가능하냐 // 160001 넣어서 메모리 터짐 아오
+
+int solution(std::vector<int>& candsa)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> cand >> pick >> tot;
-
-    vector<vector<int>> candis(tot, vector<int>(2, 0));
-    for (int i = 0; i < cand; ++i)
-    {
-        cin >> candis[i][0] >> candis[i][1];
+    int answer = 0;
+    dp[0][1][candsa[0]] = true;
+    for (int i = 1; i < cand; ++i)// i개를 고를 때
+    {    // 기저 조건
+    // [i 번째][1][i의 능력치]
+    // [i번째][0][0]
+        dp[i][1][candsa[i]] = true;
+        dp[i][0][0] = true;
+        for (int j = 1; j <= i + 1; ++j)
+        {
+            for (int a = 0; a <= tot * pick; a++)
+            {
+                // i번째 후보를 고르는 경우
+                if (a - candsa[i] >= 0 && j > 0)
+                    dp[i][j][a] |= dp[i - 1][j - 1][a - candsa[i]]; // i-1번째에 j-1개 만큼 고른 후 i의 a능력치만큼 지금 임의의 a합에서 뺀 경우가 존재하는 경우 지금의 값이 true로 가능
+                // i번째 후보를 안 고르는 경우
+                dp[i][j][a] |= dp[i - 1][j][a]; // i번째에 j만큼 고를때 a만큼의 a합을 가질 수 있는 경우는 i-1번째에 j만큼 고르면서 a만큼의 합을 가질 수 있을 경우(true)에 true 하다
+            }
+        }
     }
-    set<int> curcan;
-    BackT(0, pick, curcan, candis);
 
-    cout << maxi << endl;
+    // 가능한 경우의 A값이 나오는경우 계산
+    // 이때 최대 능력치 합 * 선택하는 후보의 수 만큼 까지만 능력치가 나올 것
+    for (int a = 0; a <= tot * pick; a++)
+    {
+        if (dp[cand - 1][pick][a])
+            answer = std::max(answer, a * (tot * pick - a));
+    }
+    return answer;
 }
 
-void BackT(int num, int left, set<int> curcan, vector<vector<int>>& candis)
+int main()
 {
-    if (num >= cand)
-    {// 마지막 후보까지 끝
-        return;
-    }
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-    // 이번 후보 넣기
-    curcan.insert(num);
-    left -= 1;
-    if (left == 0)
-    {// k개의 후보를 다 골랐다면
-        // 계산
-        int pow = 0;
-        int speed = 0;
-        for (auto& a : curcan)
-        {
-            pow += candis[a][0];
-            speed += candis[a][1];
+    std::cin >> cand >> pick >> tot;
 
-        }
-        int sum = pow * speed;
-        maxi = max(maxi, sum);
-    }
-    else
+    std::vector<int> candsa(cand, 0);
+    for (int i = 0; i < cand; ++i)
     {
-        BackT(num + 1, left, curcan, candis);
+        int a, b;
+        std::cin >> a >> b;
+        candsa[i] = a;
     }
-    // 이전 계산 롤백
-    curcan.erase(curcan.find(num));
-    left += 1;
-    // 이번 후보 거르기 => 다음 후보 탐색 시작
-    BackT(num + 1, left, curcan, candis);
 
+    std::cout << solution(candsa);
 }
