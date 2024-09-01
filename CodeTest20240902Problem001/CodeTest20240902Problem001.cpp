d15a85aa9a5519a1a886bc3c8ea020cfc9d5a830
Beomin <960322ksh@hanmail.net>
2024-09-02 02:07:10 +0900

    // 프로그래머스 고득점 킷 정렬 2번 가장 큰 수 // 쟁점: string 과 int 타입간의 변화를 잘 수행 할 수 있냐, string 타입의 함수를 잘 사용할 수 있냐, sort의 비교 연산을 위해 custom한 비교 함수를 사용 할 수 있냐

---
+++ CodeTest20240902Problem001/CodeTest20240902Problem001.cpp
@@ -0,0 +1,41 @@
+﻿// 프로그래머스 고득점 킷 정렬 2번 가장 큰 수
+// 쟁점: string 과 int 타입간의 변화를 잘 수행 할 수 있냐, string 타입의 함수를 잘 사용할 수 있냐, sort의 비교 연산을 위해 custom한 비교 함수를 사용 할 수 있냐
+// 이어지는 수에 대해 내림 차순 우선선위 a>b를 수행하기
+// 이때 어떤 수가 합쳤을 때 더 큰 수가 되는 지는 문자열로 변환 후 a+b, b+a 수행해서 더 큰 수가 나오는 경우 그 수가 sort시 더 큰 수가 되도록 한다
+// 예) 39 33이 있을 때 3933 > 3339가 true 가 되도록 하면  3933이 정렬시 앞으로 가도록 함수를 custom해서 sort() 함수의 인자로 넣는다.
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+bool sorting(string a, string b)
+{
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+
+    string answer;
+    vector<string> numStrings;
+
+    for (auto num : numbers)
+    {
+        numStrings.push_back(to_string(num));
+    }
+    sort(numStrings.begin(), numStrings.end(), sorting);
+
+    if (numStrings[0] == "0") return "0";
+    for (auto ns : numStrings)
+    {
+        answer.append(ns);
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+}
--- Samples/Samples.cpp
+++ Samples/Samples.cpp
@@ -1,66 +1,27 @@
 ﻿#include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
-#include <queue>
+
 using namespace std;
 
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    int curweight = 0;//현재 다리가 버티는 무게
-    int curtime = 1; // 시간 재는중 1초에 한번 트럭들이 이동가능
-    // 트럭 한대는 다리 길이만큼의 시간을 소모해서 다리를 건넘
-    // 트럭이 다리에 올라온 시간+다리길이의 시간에 해당 트럭이 다리를 건넘
-    // 1+2=3에 예시의 7t 트럭이 끝남
-    // 무거운거 부터 빨리 끝내는게? 
-    queue<pair<int, int>> bt;//다리 타이머
-    // 1초가 지나면 ------------
-    // 트럭이 내릴 수 있다면 트럭을 먼저 내림
-    // 다리의 현재 무게와 최대 대수 미만으로 트럭이 진행중인 경우 트럭을 추가함
-    // 트럭을 추가했다면 index를 하나 넘김
-    // 1초가 지남
+int main(void) {
+	int a[5] = { 1, 2, 3, 4, 5 };
 
-    int index = 0;
-    int curtruck = 0;
+	vector<int> vec;
 
-    curweight += truck_weights[index];
-    bt.push({ curtime + bridge_length,truck_weights[index] });
-    index++;
-    curtruck++;
-    curtime++;
-    while (!bt.empty())
-    {
-        if (curtime == bt.front().first)
-        { // 목표 도달 시간 도달 시 하차
-            auto desttruck = bt.front();
-            bt.pop();
-            curtruck--; // 현재 트럭 수 체크
-            curweight -= desttruck.second;// 현재 하중에서 제거
-        }
-        // 추가적인 트럭 운행이 가능한지 점검
-        if (index >= truck_weights.size())
-        {
-            answer = bt.back().first;
-            return answer;
-        }
-        if (curtruck < bridge_length && index < truck_weights.size())
-        {
-            if (curweight + truck_weights[index] <= weight)
-            {
-                curtruck++;
-                curweight += truck_weights[index];
-                bt.push({ curtime + bridge_length,truck_weights[index] });
-                index++;
-            }
+	for (int i = 0; i < 5; i++) {
+		vec.push_back(a[i]);
+	}
 
-        }
-        curtime++;
-    }
-}
+	vector<bool> visited(5, true);
 
+	for (int i = 0; i < vec.size() - 3; i++) visited[i] = false;
+	do {
+		for (int i = 0; i < vec.size(); i++) {
+			if (visited[i])	cout << vec[i] << " ";
+		}
+		cout << endl;
+	} while (next_permutation(visited.begin(), visited.end()));
 
-int main()
-{
-    int answer = solution(2, 10, {7,4,5,6});
-    cout << answer;
+	return 0;
 }
