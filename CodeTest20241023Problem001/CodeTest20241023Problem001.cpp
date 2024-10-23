7d411519f2b8afa907c0b53285e1f0e878535e6b
Beomin <960322ksh@hanmail.net>
2024-10-23 17:44:08 +0900

    이분탐색 연습 백준 두 용액 세 용액

---
+++ CodeTest20241023Problem001/CodeTest20241023Problem001.cpp
@@ -0,0 +1,49 @@
+﻿// 백준 2470 두 용액
+// 투포인터 탐색
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+int N;
+using namespace std;
+int main()
+{
+    cin >> N;
+    vector <long long> nums(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> nums[i];
+    }
+    vector<long long> best2(2, 0);
+    sort(nums.begin(), nums.end());
+
+    int left=0;
+    int right=nums.size()-1;
+    long long sum;
+    long long cursum=LLONG_MAX;
+
+
+    while (left < right)
+    {
+        sum = nums[left] + nums[right];
+        if (cursum >= abs(sum))
+        {
+            cursum = abs(sum);
+            best2[0] = nums[left];
+            best2[1] = nums[right];
+        }
+       
+        if (sum > 0)
+        {
+            right--;
+        }
+        else
+        {
+            left++;
+        }
+
+    }
+    sort(best2.begin(), best2.end());
+    cout << best2[0] <<' ' << best2[1] << endl;;
+}
---
+++ CodeTest20241023Problem002/CodeTest20241023Problem002.cpp
@@ -0,0 +1,53 @@
+﻿// 백준 2473 세 용액
+// 투포인터 탐색
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+int N;
+using namespace std;
+int main()
+{
+    cin >> N;
+    vector <long long> nums(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> nums[i];
+    }
+    vector<long long> best2(3, 0);
+    sort(nums.begin(), nums.end());
+
+    long long sum;
+    long long cursum = LLONG_MAX;
+    
+    for (int k = 0; k < nums.size() - 2; k++)
+    {
+        int left = k+1;
+        int right = nums.size() - 1;
+        while (left < right)
+        {
+            sum = nums[k] + nums[left] + nums[right];
+            if (cursum >= abs(sum))
+            {
+                cursum = abs(sum);
+                best2[0] = nums[left];
+                best2[1] = nums[right];
+                best2[2] = nums[k];
+            }
+
+            if (sum > 0)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+
+        }
+
+    }
+    sort(best2.begin(), best2.end());
+    cout << best2[0] << ' ' << best2[1] << ' ' << best2[2] << endl;;
+}
