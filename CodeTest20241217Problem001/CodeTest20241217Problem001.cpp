27f6c6b77dcb02ecd348e9987df39463ed74dd0f
Sang-Hyun-Kim <960322ksh@hanmail.net>
2024-12-27 09:57:33 +0900

    프로그래머스 문제 업로드 // 공원산책 // 소수찾기

---
+++ CodeTest20241217Problem001/CodeTest20241217Problem001.cpp
@@ -0,0 +1,94 @@
+﻿#include <string>
+#include <vector>
+#include <array>
+#include <iostream>
+// 프로그래머스 공원산책
+using namespace std;
+array<int, 4> dx = { -1,1,0,0 };
+array<int, 4> dy = { 0,0,-1,1 };
+int R, C;
+bool InRange(int x, int y)
+{
+    if (0 <= x && 0 <= y && x < R && y < C)
+    {
+        return true;
+    }
+    return false;
+}
+
+vector<int> solution(vector<string> park, vector<string> routes) {
+    vector<int> answer;
+
+    R = park.size();
+    C = park[0].size();
+    pair<int, int> srt;
+    for (int i = 0; i < R; ++i)
+    {
+        for (int j = 0; j < C; ++j)
+        {
+            if (park[i][j] == 'S')
+            {
+                srt = { i,j };
+            }
+        }
+    }
+    int cx = srt.first;
+    int cy = srt.second;
+    for (auto r : routes)
+    {
+
+        auto way = r[0];
+        auto valc = r[2]-48;//'0'
+        int wi = 0;
+        if (way == 'N')
+        {
+            wi = 0;
+        }
+        else if (way == 'S')
+        {
+            wi = 1;
+        }
+        else if (way == 'W')
+        {
+            wi = 2;
+        }
+        else
+        {
+            wi = 3;
+        }
+        bool flag = true;
+        int nx = cx;
+        int ny = cy;
+        for (int i = 0; i < valc; ++i)
+        {
+            nx+= dx[wi];
+            ny+= dy[wi];
+            if (!InRange(nx, ny))
+            {
+                flag = false;
+            }
+            else
+            {
+                if (park[nx][ny] == 'X')
+                {
+                    flag = false;
+                }
+            }
+        }
+        if (flag)
+        {
+            cx = nx;
+            cy = ny;
+        }
+    }
+    answer.push_back(cx);
+    answer.push_back(cy);
+    return answer;
+}
+
+int main()
+{
+    vector<int> a = solution({"OSO", "OOO", "OXO","000"
+        }, { "E 2", "S 3", "W 1" });
+    int f = 3;
+}
---
+++ CodeTest20241227Problem001/CodeTest20241227Problem001.cpp
@@ -0,0 +1,56 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cmath>
+using namespace std;
+//프로그래머스 소수찾기
+// 실수한 부분
+// numbers의 사이즈가 n개면 1~n개를 선택한 숫자를 소수찾기해야하는데 전부 순열로 돌려버림(제일 중요한 처리)
+// 소수찾기 함수의 예외처리 2이하의 수에 대해서 찾기를 빼먹음
+// 소수찾기는 제곱근"까지" 해야하는데 부등호를 < 넣어버림...
+int answer = 0;
+set<int> ansset;
+bool isPrime(int _num);
+int solution(string numbers) {
+
+    sort(numbers.begin(), numbers.end());
+    int numsize = numbers.size();
+    do
+    {
+        string tmpstr;
+        for (int i = 0; i < numsize; ++i)
+        {
+            tmpstr += numbers[i];
+            int tmpnum = stoi(tmpstr);
+            if (isPrime(tmpnum))
+            {
+                ansset.insert(tmpnum);
+            }
+
+        }
+
+
+    } while (next_permutation(numbers.begin(), numbers.end()));
+    return ansset.size();
+}
+
+
+
+bool isPrime(int _num)
+{
+    if (_num < 2)
+        return false;
+    for (int i = 2; i <= sqrt(_num); ++i)
+    {
+        if (_num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+}
+
